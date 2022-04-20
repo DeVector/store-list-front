ed103bc2effe7d8d19764aed975b80168995efdb
DeVector <diniz.vito@gmail.com>
2022-04-20 15:50:26 -0300

    Add components e intalando o bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.modules';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { HardwareListComponent } from './components/store/hardware-list/hardware-list.component';
+import { FilterComponent } from './components/store/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HardwareListComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
